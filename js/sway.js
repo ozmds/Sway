896c39fb7a15fd6d60cad96fcc8c26a507d591d8
Athavan Karunakaran <Athavan Karunakaran>
2017-06-15 20:14:07 -0400

    Sway.js Lines 1-247 commented

--- js/sway.js
+++ js/sway.js
@@ -13,6 +13,8 @@ const TIME_INTERVAL = 15;
 const PEN_START_TIME = 1500;
 const PEN_DECREMENT_TIME = 0.05 * PEN_START_TIME;
 
+const ORB_FREQUENCY = 1500;
+
 var PRIMARY_COLOUR = BLUE;
 var DEST_COLOUR = PRIMARY_COLOUR;
 
@@ -48,6 +50,11 @@ function Sway(cnv) {
 	this.getScore = function() {
 		/* Return Score */
 		return this.score;
+	}
+	
+	this.setScore = function(x) {
+		/* Set Score to x */
+		this.score = x;
 	}
 	
 	this.incrementScore = function() {
@@ -125,72 +132,41 @@ function Sway(cnv) {
 		this.pen.draw();
 	}
 	
-	this.manageOrbs = function() {
-		/* Manage the all of the orbs currently on the screen */
-		
-		var i;
-		if (time_counter >= 1500) {
-			time_counter = 0;
-			
-			var d = new Diamond(this.cnv.width * 0.035, this.ctx, this.cnv, this.score, this.speed);
-			d.place(this.pen.getArm().getOldLen(), this.pen.getPen().getR()); 
-			this.orbList.push(d);
+	this.createOrb = function() {
+		/* Create a New Orb */
+		var d = new Diamond(this.cnv.width * 0.035, this.ctx, this.cnv, this.score, this.speed);
+		d.place(this.pen.getArm().getOldLen(), this.pen.getPen().getR()); 
+		this.orbList.push(d);
+	}
+	
+	this.checkHit = function(orb, pen1, pen2) {
+		/* Check if Orb has been hit by Pendulum */
+		if (orb.checkHitPen(pen1.getX(), pen1.getY(), pen1.getR() * this.pen.getSpikeHeight())) {
+			return true;
+		} else if (orb.checkHitPen(pen2.getX(), pen2.getY(), pen2.getR())) {
+			return true;	
 		}
-		
-		for (i = 0; i < this.orbList.length; i++) {
-			
-			if (this.status != REGULAR) {
-				if (this.status != SLOW_DOWN) {
-					this.orbList[i].setType(REGULAR);
-					this.orbList[i].setImage();
-				} else {
-					if (this.orbList[i].getType() != POISON) {
-						this.orbList[i].setType(REGULAR);
-						this.orbList[i].setImage();
-					}
-				}
-			}
-			
-			this.orbList[i].move(1);
-			
-			if (this.orbList[i].getY() > (this.cnv.height + this.orbList[i].getR() * 2)) {
-				if (this.orbList[i].getType() == REGULAR) {
-					this.orbList.splice(i, 1);
-					this.score = 0;
-				}
-			} else if ((this.orbList[i].checkHitPen(this.pen.getPen().getX(), 
-													this.pen.getPen().getY(), 
-													this.pen.getPen().getR() * this.pen.getSpikeHeight())) ||
-						(this.orbList[i].checkHitPen(this.pen.getSPen().getX(), 
-													this.pen.getSPen().getY(), 
-													this.pen.getSPen().getR()))) {
-				this.hitList.push(this.orbList[i]);
-				
-				if (this.orbList[i].getType() == POISON) {
-					this.score = 0; 
-				} else if (this.orbList[i].getType() == SLOW_DOWN) {
-					this.setStatus(SLOW_DOWN);					
-				} else if (this.orbList[i].getType() == BALLOON) {
-					this.setStatus(BALLOON);
-				} else if (this.orbList[i].getType() == SPIKE) {
-					this.setStatus(SPIKE);
-				}
-				
-				if (this.orbList[i].getType() == REGULAR) {
-					this.incrementScore();
-					if ((this.score % 10 == 0) && (this.score <= 200)) {
-						pen_time = PEN_START_TIME - (this.score / 10) * PEN_DECREMENT_TIME;
-						this.pen.calcPenSpeed(pen_time);
-					}
-				}
-				
-				this.orbList.splice(i, 1);
-				
-			} else {
-				this.orbList[i].draw();
-			}
+
+		return false;
+	}
+	
+	this.setStatusByOrb = function(orb_type) {
+		/* Set Game Status by Orb Return Type */
+		if (orb_type != REGULAR) {
+			this.setStatus(orb_type);
 		}
-		
+	}
+	
+	this.incrementPenSpeed = function(score) {
+		/* Increase Speed of Pendulum */
+		if ((score % 10 == 0) && (score <= 200)) {
+			pen_time = PEN_START_TIME - (score / 10) * PEN_DECREMENT_TIME;
+			this.pen.calcPenSpeed(pen_time);
+		}
+	}
+	
+	this.manageHitList = function(i) {
+		/* Move All Orbs That Have Been Hit */
 		for (i = 0; i < this.hitList.length; i++) {
 			this.hitList[i].updateHitTimer(TIME_INTERVAL);
 			
@@ -201,6 +177,56 @@ function Sway(cnv) {
 				this.hitList[i].draw();
 			}
 		}
+	}
+	
+	this.manageOrbs = function() {
+		/* Manage the all of the orbs currently on the screen */
+		var i;
+		
+		/* Create a New Orb */
+		if (time_counter >= ORB_FREQUENCY) {
+			time_counter = 0;
+			this.createOrb();
+		}
+		
+		/* For Each Orb */
+		for (i = 0; i < this.orbList.length; i++) {
+			
+			/* Change All Orbs to Regular during Power-Up */
+			if (this.status != REGULAR) {
+				this.orbList[i].setType(REGULAR);
+				this.orbList[i].setImage();
+			}
+			
+			/* Move Orb */
+			this.orbList[i].move(1);
+			
+			/* Check if Orb Hits Floor */
+			if (this.orbList[i].getY() > (this.cnv.height + this.orbList[i].getR() * 2)) {
+				if (this.orbList[i].getType() == REGULAR) {
+					this.orbList.splice(i, 1);
+					this.score = 0;
+				}
+			/* Check if Orb Hits Pendulum */ 
+			} else if (this.checkHit(this.orbList[i], this.pen.getPen(), this.pen.getSPen())) {
+				this.hitList.push(this.orbList[i]);
+				
+				this.setStatusByOrb(this.orbList[i].getType());
+				
+				if (this.orbList[i].getType() == REGULAR) {
+					this.incrementScore();
+					this.incrementPenSpeed(this.score);
+				}
+				
+				this.orbList.splice(i, 1);
+				
+			} else {
+				this.orbList[i].draw();
+			}
+		}
+		
+		/* Manage Orbs Already Hit */
+		this.manageHitList(i);
 		
 		time_counter = time_counter + TIME_INTERVAL;
 	}
@@ -348,6 +374,9 @@ function startGame() {
 			if (!swayGame.getPen().startSpike()) {
 				swayGame.setStatus(REGULAR);
 			}
+		} else if (swayGame.getStatus() == POISON) {
+			swayGame.setScore(0);
+			swayGame.setStatus(REGULAR);
 		}
 		
 		swayGame.move();
