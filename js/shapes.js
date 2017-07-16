2e6187109d0a188a118c1f3c73709cb83ee4c018
Athavan Karunakaran <athavan.karunakaran@gmail.com>
2017-07-16 13:39:02 -0400

    Highscore implemented

--- js/shapes.js
+++ js/shapes.js
@@ -37,55 +37,55 @@ class Circle {
 		this.sp = 0;
 		this.oldR = r;
 	}
-	
+
 	setDir(x) {
 		this.dir = x;
 	}
-	
+
 	getDir() {
 		return this.dir;
 	}
-	
+
 	setCol(x) {
 		this.col = x;
 	}
-	
+
 	getOldR() {
 		return this.oldR;
 	}
-	
+
 	setX(x) {
 		this.x = x;
 	}
-	
+
 	setR(x) {
 		this.r = x;
 	}
-	
+
 	setY(x) {
 		this.y = x;
 	}
-	
+
 	getX() {
 		return this.x;
 	}
-	
+
 	getY() {
 		return this.y;
 	}
-	
+
 	getR() {
 		return this.r;
-	}	
-	
+	}
+
 	getDeg() {
 		return this.deg;
 	}
-	
+
 	setDeg(x) {
 		this.deg = x;
 	}
-	
+
 	flip() {
 		if (this.dir == LEFT) {
 			this.dir = RIGHT;
@@ -93,72 +93,72 @@ class Circle {
 			this.dir = LEFT;
 		}
 	}
-	
-	drawInnerCircle() {		
+
+	drawInnerCircle() {
 		this.ctx.lineWidth = this.owid * 0.80;
 		this.ctx.strokeStyle = this.ocol;
-		
-		this.ctx.beginPath(); 
+
+		this.ctx.beginPath();
 		this.ctx.arc(this.x, this.y, this.r * 0.65, 0.0 * Math.PI + (this.sp * 2.0 * Math.PI),
 													2.0 * Math.PI * 0.85 + (this.sp * 2.0 * Math.PI));
 		this.ctx.stroke();
 		this.ctx.closePath();
 	}
-	
+
 	spin() {
 		this.sp = this.sp + 0.008;
 	}
-	
+
 	drawSpikes(a) {
 		var i;
 		var j;
-		
+
 		var x;
 		var y;
-		
+
 		this.ctx.strokeStyle = this.ocol;
 		this.ctx.lineWidth = this.owid * 0.8;
 		this.ctx.fillStyle = this.col;
-		
+
 		for (i = 0; i < 6; i++) {
 			this.ctx.beginPath();
 			for (j = 0; j < 3; j++) {
 				x = this.r * Math.cos((i + j / 2) * Math.PI / 3 + (this.sp * Math.PI));
 				y = this.r * Math.sin((i + j / 2) * Math.PI / 3 + (this.sp * Math.PI));
-				
+
 				if (j == 1) {
 					x = a * x;
 					y = a * y;
 				}
-				
+
 				x = x + this.x;
 				y = y + this.y;
-				
+
 				if (j == 0) {
 					this.ctx.moveTo(x, y);
 				} else {
 					this.ctx.lineTo(x, y);
 				}
 			}
-			
+
 			this.ctx.stroke();
 			this.ctx.fill();
 			this.ctx.closePath();
 		}
 	}
-	
+
 	draw() {
 		this.ctx.fillStyle = this.col;
 		this.ctx.lineWidth = this.owid;
 		this.ctx.strokeStyle = this.ocol;
-			
+
 		this.ctx.beginPath();
 		this.ctx.arc(this.x, this.y, this.r, 0.0 * Math.PI, 2.0 * Math.PI);
 		this.ctx.fill();
 		this.ctx.stroke();
 		this.ctx.closePath();
 	}
-	
+
 	hitWall(pad) {
 		if (this.x < this.r + pad) {
 			return true;
@@ -166,25 +166,25 @@ class Circle {
 			return true;
 		}
 	}
-	
+
 	move(speed, armLength, cen, pad) {
 		if (this.dir == LEFT) {
 			this.deg -= speed;
 		} else {
 			this.deg += speed;
 		}
-		
+
 		this.x = cen.getX() + Math.cos(this.deg) * armLength;
 		this.y = cen.getY() - Math.sin(this.deg) * armLength;
-		
+
 		if (this.hitWall(pad)) {
 			this.flip();
-			if (this.dir == LEFT) { 
+			if (this.dir == LEFT) {
 				this.deg -= 2 * speed;
 			} else {
 				this.deg += 2 * speed;
 			}
-			
+
 			this.x = cen.getX() + Math.cos(this.deg) * armLength;
 			this.y = cen.getY() - Math.sin(this.deg) * armLength;
 		}
@@ -202,36 +202,36 @@ class Line {
 		this.ctx = ctx;
 		this.cnv = cnv;
 		this.len = endY - stY;
-		this.oldLen = endY - stY; 
+		this.oldLen = endY - stY;
 	}
-	
+
 	draw() {
 		this.ctx.lineWidth = this.wid;
 		this.ctx.strokeStyle = this.col;
-		
+
 		this.ctx.beginPath();
 		this.ctx.moveTo(this.stX, this.stY);
 		this.ctx.lineTo(this.endX, this.endY);
 		this.ctx.stroke();
 		this.ctx.closePath();
 	}
-	
+
 	getOldLen() {
 		return this.oldLen;
 	}
-	
+
 	getLen() {
 		return this.len;
 	}
-	
+
 	setLen(x) {
 		this.len = x;
 	}
-	
+
 	setEndX(x) {
 		this.endX = x;
 	}
-	
+
 	setEndY(y) {
 		this.endY = y;
 	}
@@ -250,24 +250,24 @@ class Diamond {
 		this.aspectRatio = 0;
 		this.img = null;
 		this.speed = speed;
-		
+
 		var typeInt = Math.random() * 100;
 		var x16level = x16start;
 		var x13level = x13start;
-		
+
 		if (score % 20 == 0 && score >= 20) {
 			if (score <= 60) {
 				x16level = x16start + 5 * (score / 20);
 				x13level = x13start + 5 * (score / 20);
 			}
 		}
-		
+
 		if (typeInt < x16level) {
 			this.speed = this.speed * 3;
 		} else if (typeInt < x16level + x13level) {
 			this.speed = this.speed * 2;
 		}
-		
+
 		typeInt = Math.random() * 100;
 
 		if (typeInt < 20) {
@@ -281,10 +281,10 @@ class Diamond {
 		} else {
 			this.type = SPIKE;
 		}
-		
+
 		this.setImage();
 	}
-	
+
 	setImage() {
 		if (this.type == REGULAR) {
 			this.img = crystal;
@@ -297,79 +297,79 @@ class Diamond {
 		} else if (this.type == SPIKE) {
 			this.img = knife;
 		}
-		
+
 		this.aspectRatio = this.img.height / this.img.width;
 	}
-	
+
 	getType() {
 		return this.type;
 	}
-	
+
 	setType(x) {
 		this.type = x;
 	}
-	
+
 	getHitTimer() {
 		return this.hitTimer;
-	}	
-	
+	}
+
 	updateHitTimer(interval) {
 		this.hitTimer = this.hitTimer + interval;
 	}
-	
+
 	getY() {
 		return this.y;
 	}
-	
+
 	getR() {
 		return this.r;
 	}
-	
+
 	checkHitPen(x, y, r) {
 		var dist_x = Math.pow(this.x - x, 2);
 		var dist_y = Math.pow(this.y - y, 2);
-		
+
 		if (Math.pow(r + this.r * 2, 2) > (dist_x + dist_y)) {
 			return true;
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 	hitOrb() {
 		if (this.hitTimer > 0) {
 			this.r = this.r * 0.98;
 		}
 	}
-	
+
 	draw() {
-		this.hitOrb();		
+		this.hitOrb();
 		this.ctx.lineWidth = this.r * 0.30;
 		this.ctx.strokeStyle = this.col;
 		this.ctx.fillStyle = PRIMARY_COLOUR;
-		
+
 		this.ctx.beginPath();
 		this.ctx.arc(this.x, this.y, this.r * 2, 0.0 * Math.PI, 2.0 * Math.PI);
 		this.ctx.fill();
 		this.ctx.stroke();
 		this.ctx.closePath();
-		
+
 		this.ctx.drawImage(this.img, this.x - this.r, this.y - this.r * this.aspectRatio, 2 * this.r, 2 * this.r * this.aspectRatio);
 	}
-	
+
 	place(armLength, rad) {
 		var range = 2 * (armLength + rad);
-		
+
 		if (this.cnv.width < range) {
 			this.x = Math.round(Math.random() * (this.cnv.width - 8 * this.r)) + 4 * this.r;
 		} else {
 			this.x = Math.round(Math.random() *	 range) + (0.5 * (this.cnv.width - range));
 		}
-		
+
 		this.draw();
 	}
-	
+
 	move(x) {
 		this.y += this.speed * x;
 	}
@@ -389,55 +389,55 @@ class Pendulum {
 		this.sPen = null;
 		this.speed = null;
 	}
-	
+
 	calcPenSpeed(penTime) {
 		this.speed = (this.endRange - this.startRange) / (penTime / TIME_INTERVAL);
 	}
-	
+
 	setCol(x) {
 		this.pen.setCol(x);
 		this.cen.setCol(x);
 		this.sPen.setCol(x);
 	}
-	
+
 	setSArm(x) {
 		this.sArm = x;
 	}
-	
+
 	setSPen(x) {
 		this.sPen = x;
 	}
-	
+
 	getSPen() {
 		return this.sPen;
 	}
-	
+
 	getSpikeHeight() {
 		return this.spikeHeight;
 	}
-	
+
 	setRange(width, padding) {
 		var span = Math.asin((width - (this.pen.getR() + padding)) / this.arm.getOldLen());
 		this.startRange = Math.PI * 1.5 - span;
 		this.endRange = Math.PI * 1.5 + span;
 	}
-	
+
 	setSmallLen(cnv) {
-		this.smallLen = (cnv.width / 2) - this.pen.getR() - PADDING;	
+		this.smallLen = (cnv.width / 2) - this.pen.getR() - PADDING;
 	}
-	
+
 	getPen() {
 		return this.pen;
 	}
-	
+
 	getCen() {
 		return this.cen;
 	}
-	
+
 	getArm() {
 		return this.arm;
 	}
-	
+
 	draw() {
 		this.pen.spin();
 		this.arm.draw();
@@ -448,30 +448,30 @@ class Pendulum {
 		this.pen.draw();
 		this.pen.drawInnerCircle();
 	}
-	
+
 	move(status) {
 		this.pen.move(this.speed, this.arm.getLen(), this.cen, PADDING);
-		
+
 		if (status == BALLOON) {
 			this.sPen.move(-this.speed, this.sArm.getLen(), this.cen, PADDING);
 		} else {
 			this.sPen.move(this.speed, this.sArm.getLen(), this.cen, PADDING);
 		}
-		
+
 		this.arm.setEndX(this.pen.getX());
 		this.arm.setEndY(this.pen.getY());
 		this.sArm.setEndX(this.sPen.getX());
 		this.sArm.setEndY(this.sPen.getY());
 	}
-	
+
 	startShrink() {
-		var deg = null; 
+		var deg = null;
 		if (this.pen.getDeg() > 0) {
 			deg = this.pen.getDeg() % (2 * Math.PI);
 		} else {
 			deg = (2 * Math.PI) - Math.abs(this.pen.getDeg() % (2 * Math.PI));
 		}
-		 
+
 		if (this.timer < 15000) {
 			DEST_COLOUR = RED;
 			if (this.arm.getLen() > this.smallLen) {
@@ -490,16 +490,16 @@ class Pendulum {
 				return false;
 			}
 		}
-		
+
 		this.timer += TIME_INTERVAL;
-		
+
 		return true;
 	}
-	
+
 	startBalloon() {
 		var dist_x = Math.pow(Math.abs(this.pen.getX() - this.sPen.getX()), 2);
 		var dist_y = Math.pow(Math.abs(this.pen.getY() - this.sPen.getY()), 2);
-		
+
 		if (this.timer > 15000) {
 			DEST_COLOUR = BLUE;
 			if (dist_x + dist_y < Math.pow(this.pen.getR(), 2)) {
@@ -511,12 +511,12 @@ class Pendulum {
 		} else {
 			DEST_COLOUR = GREEN;
 		}
-		
+
 		this.timer += TIME_INTERVAL;
-		
+
 		return true;
 	}
-	
+
 	startSpike() {
 		if (this.timer < 15000) {
 			DEST_COLOUR = PURPLE;
@@ -526,7 +526,7 @@ class Pendulum {
 				this.pen.setR(this.pen.getR() * 1.01);
 				this.sPen.setR(this.sPen.getR() * 1.01);
 			}
-			
+
 			if (this.spikeHeight < 2) {
 				this.spikeHeight = this.spikeHeight * 1.01;
 			}
@@ -544,9 +544,9 @@ class Pendulum {
 				return false;
 			}
 		}
-		
+
 		this.timer += TIME_INTERVAL;
-		
+
 		return true;
-	}	
+	}
 }
--- js/sway.js
+++ js/sway.js
@@ -26,7 +26,7 @@ var DEST_COLOUR = PRIMARY_COLOUR;
 
 var time_counter = 0;
 
-var orb_time = ORB_START_TIME; 
+var orb_time = ORB_START_TIME;
 var pen_time = PEN_START_TIME;
 
 function Sway(cnv) {
@@ -39,130 +39,138 @@ function Sway(cnv) {
 	this.score = 0;
 	this.speed = 0;
 	this.status = REGULAR;
-	
+
 	this.cnv.style.top = (MARGIN).toString() + 'px';
 	this.cnv.style.left = (MARGIN).toString() + 'px';
-	
+
 	this.getStatus = function() {
 		/* Return Status */
 		return this.status;
 	}
-	
+
 	this.setStatus = function(x) {
 		/* Set Status to x */
 		this.status = x;
 	}
-	
+
 	this.getScore = function() {
 		/* Return Score */
 		return this.score;
 	}
-	
+
 	this.setScore = function(x) {
 		/* Set Score to x */
 		this.score = x;
 	}
-	
+
 	this.incrementScore = function() {
 		/* Increase Score By 1 */
 		this.score += 1;
+
+		if (this.score > window.localStorage.getItem('highscore')) {
+			window.localStorage.setItem('highscore', this.score);
+		}
 	}
 
 	this.getOldHeight = function() {
 		/* Return Old Height */
 		return this.old_height;
 	};
-	
+
 	this.setOldHeight = function(x) {
 		/* Set Old Height to x */
 		this.old_height = x;
 	}
-	
+
 	this.getCtx = function() {
 		/* Return Context */
 		return this.ctx;
 	}
-	
+
 	this.getCnv = function() {
 		/* Return Canvas */
 		return this.cnv;
 	}
-	
+
 	this.getPen = function() {
 		/* Return Pendulum */
 		return this.pen;
 	}
-	
+
 	this.calculateSpeed = function() {
 		/* Calculate Speed of Orb based on orb_time */
 		this.speed = Math.round(this.cnv.height / (orb_time / TIME_INTERVAL));
 	}
-	
+
 	this.initVariable = function() {
 		/* Initialize all the main variables */
-		
+
 		/* Create The Pendulum */
 		var cen_height = 0.60;
 		var pen_rad = this.cnv.width * 0.1;
-		
+
 		var cen_x = this.cnv.width / 2;
 		var cen_y = this.cnv.height * cen_height;
-		
+
 		var pen_x = this.cnv.width / 2;
 		var pen_y = this.cnv.height - (pen_rad + PADDING);
-		
-		var arm = new Line(cen_x, cen_y, pen_x, pen_y, this.cnv.width * 0.015, SECONDARY_COLOUR, this.ctx, this.cnv); 
+
+		var arm = new Line(cen_x, cen_y, pen_x, pen_y, this.cnv.width * 0.015, SECONDARY_COLOUR, this.ctx, this.cnv);
 		var cen = new Circle(cen_x, cen_y, pen_rad / 2, PRIMARY_COLOUR, this.cnv.width * 0.0175, SECONDARY_COLOUR, this.ctx, this.cnv);
 		var pen = new Circle(pen_x, pen_y, pen_rad, PRIMARY_COLOUR, this.cnv.width * 0.02, SECONDARY_COLOUR, this.ctx, this.cnv);
-		
+
 		var sArm = new Line(cen_x, cen_y, pen_x, pen_y, this.cnv.width * 0.015, SECONDARY_COLOUR, this.ctx, this.cnv);
 		var sPen = new Circle(pen_x, pen_y, pen_rad, PRIMARY_COLOUR, this.cnv.width * 0.02, SECONDARY_COLOUR, this.ctx, this.cnv);
-		
+
 		this.pen = new Pendulum(cen, pen, arm);
-		
+
 		this.pen.setSArm(sArm);
 		this.pen.setSPen(sPen);
-		
+
 		/* Create Boundaries for Pendulum */
 		this.pen.setSmallLen(this.cnv);
-		this.pen.setRange(this.cnv.width / 2, PADDING); 
-		
+		this.pen.setRange(this.cnv.width / 2, PADDING);
+
 		/* Calculate Speed for Pendulum and Orb */
 		this.calculateSpeed();
 		this.pen.calcPenSpeed(pen_time);
-	};	 
-	
+
+		if (window.localStorage.getItem('highscore') == null) {
+			window.localStorage.setItem('highscore', this.score);
+		}
+	};
+
 	this.move = function() {
 		/* Move the Pendulum as a Whole */
 		this.pen.move(this.status);
 		this.pen.draw();
 	}
-	
+
 	this.createOrb = function() {
 		/* Create a New Orb */
 		var d = new Diamond(this.cnv.width * 0.035, this.ctx, this.cnv, this.score, this.speed);
-		d.place(this.pen.getArm().getOldLen(), this.pen.getPen().getR()); 
+		d.place(this.pen.getArm().getOldLen(), this.pen.getPen().getR());
 		this.orbList.push(d);
 	}
-	
+
 	this.checkHit = function(orb, pen1, pen2) {
 		/* Check if Orb has been hit by Pendulum */
 		if (orb.checkHitPen(pen1.getX(), pen1.getY(), pen1.getR() * this.pen.getSpikeHeight())) {
 			return true;
 		} else if (orb.checkHitPen(pen2.getX(), pen2.getY(), pen2.getR())) {
-			return true;	
+			return true;
 		}
 
 		return false;
 	}
-	
+
 	this.setStatusByOrb = function(orb_type) {
 		/* Set Game Status by Orb Return Type */
 		if (orb_type != REGULAR) {
 			this.setStatus(orb_type);
 		}
 	}
-	
+
 	this.incrementPenSpeed = function(score) {
 		/* Increase Speed of Pendulum */
 		if ((score % 10 == 0) && (score <= 100)) {
@@ -170,7 +178,7 @@ function Sway(cnv) {
 			this.pen.calcPenSpeed(pen_time);
 		}
 	}
-	
+
 	this.incrementOrbTime = function(score) {
 		/* Increase the speed of all orbs */
 		if ((score % 10 == 0) && (score <= 200)) {
@@ -178,19 +186,19 @@ function Sway(cnv) {
 			this.calculateSpeed();
 		}
 	}
-	
+
 	this.incrementOrbFrequency = function(score) {
 		/* Increase Frequency of all orbs */
 		if ((score % 15 == 0) && (score <= 300)) {
 			orb_frequency = ORB_FREQ_START_TIME - (score / 15) * ORB_FREQ_DECREMENT_TIME;
 		}
 	}
-	
+
 	this.manageHitList = function(i) {
 		/* Move All Orbs That Have Been Hit */
 		for (i = 0; i < this.hitList.length; i++) {
 			this.hitList[i].updateHitTimer(TIME_INTERVAL);
-			
+
 			if (this.hitList[i].getHitTimer() > 2000) {
 				this.hitList.splice(i, 1);
 			} else {
@@ -199,71 +207,71 @@ function Sway(cnv) {
 			}
 		}
 	}
-	
+
 	this.manageOrbs = function() {
 		/* Manage the all of the orbs currently on the screen */
 		var i;
-		
+
 		/* Create a New Orb */
 		if (time_counter >= orb_frequency) {
 			time_counter = 0;
 			this.createOrb();
 		}
-		
+
 		/* For Each Orb */
 		for (i = 0; i < this.orbList.length; i++) {
-			
+
 			/* Change All Orbs to Regular during Power-Up */
 			if (this.status != REGULAR) {
 				this.orbList[i].setType(REGULAR);
 				this.orbList[i].setImage();
 			}
-			
+
 			/* Move Orb */
 			this.orbList[i].move(1);
-			
+
 			/* Check if Orb Hits Floor */
 			if (this.orbList[i].getY() > (this.cnv.height + this.orbList[i].getR() * 2)) {
 				if (this.orbList[i].getType() == REGULAR) {
 					this.orbList.splice(i, 1);
 					this.score = 0;
 				}
-			/* Check if Orb Hits Pendulum */ 
+			/* Check if Orb Hits Pendulum */
 			} else if (this.checkHit(this.orbList[i], this.pen.getPen(), this.pen.getSPen())) {
 				this.hitList.push(this.orbList[i]);
-				
+
 				this.setStatusByOrb(this.orbList[i].getType());
-				
+
 				if (this.orbList[i].getType() == REGULAR) {
 					this.incrementScore();
 					this.incrementPenSpeed(this.score);
 					this.incrementOrbTime(this.score);
 					this.incrementOrbFrequency(this.score);
 				}
-				
+
 				this.orbList.splice(i, 1);
-				
+
 			} else {
 				this.orbList[i].draw();
 			}
 		}
-		
+
 		/* Manage Orbs Already Hit */
 		this.manageHitList(i);
-		
+
 		time_counter = time_counter + TIME_INTERVAL;
 	}
 }
 
 Sway.prototype.background = function() {
 	/*Configure Basics of Screen Canvas */
-	
+
 	this.cnv.height = (window.innerHeight - 10) * window.devicePixelRatio;
 	this.cnv.width = (window.innerWidth - 10) * window.devicePixelRatio;
-		
+
 	this.cnv.style.width = (this.cnv.width / window.devicePixelRatio).toString() + 'px';
 	this.cnv.style.height = (this.cnv.height / window.devicePixelRatio).toString() + 'px';
-		
+
 	document.body.style.backgroundColor = PRIMARY_COLOUR;
 	this.cnv.style.backgroundColor = PRIMARY_COLOUR;
 };
@@ -271,16 +279,16 @@ Sway.prototype.background = function() {
 function changeColours(colour1, colour2) {
 	/* Slowly return fading colours from colour1 to colour2 */
 	var i;
-	
+
 	var c1 = [];
 	var c2 = [];
-	
+
 	var newcol = []
-	
+
 	for (i = 0; i < 3; i++) {
 		c1.push(parseInt(colour1.slice(2 * i + 1, 2 * i + 3), 16));
 		c2.push(parseInt(colour2.slice(2 * i + 1, 2 * i + 3), 16));
-		
+
 		if (c1[i] > c2[i]) {
 			newcol.push(c1[i] - 1);
 		} else if (c1[i] == c2[i]) {
@@ -288,79 +296,79 @@ function changeColours(colour1, colour2) {
 		} else {
 			newcol.push(c1[i] + 1);
 		}
-		
+
 		if (newcol[i] == 0) {
 			newcol[i] = '00';
 		} else if (newcol[i] < 16) {
 			newcol[i] = '0' + newcol[i].toString(16);
 		}
 	}
-	
+
 	PRIMARY_COLOUR = '#' + newcol[0].toString(16) + newcol[1].toString(16) + newcol[2].toString(16);
-	
+
 	return true;
 }
 
 function handleClick(event_x, event_y, sway) {
 	/* React to a click */
 	sway.getPen().getPen().flip();
-	
-	if (sway.getStatus() != BALLOON) {	
+
+	if (sway.getStatus() != BALLOON) {
 		sway.getPen().getSPen().flip();
 	}
 }
 
 function drawPauseButton(x, y, side_len, colour, context) {
 	/* Draw a Pause Button */
-	var bar_width = side_len / 3; 
-	context.fillStyle = colour; 
-	
-	context.fillRect(x, y, bar_width, side_len); 
-	context.fillRect(x + 2 * bar_width, y, bar_width, side_len); 
+	var bar_width = side_len / 3;
+	context.fillStyle = colour;
+
+	context.fillRect(x, y, bar_width, side_len);
+	context.fillRect(x + 2 * bar_width, y, bar_width, side_len);
 }
 
 function updateScore(c, ctx, score, highscore, colour) {
 	/* Write the score on the canvas */
-	ctx.textBaseline = 'middle'; 
-	ctx.textAlign = 'end'; 
-	ctx.fillStyle = colour; 
-	
-	var font_size = c.width * 0.13; 
+	ctx.textBaseline = 'middle';
+	ctx.textAlign = 'end';
+	ctx.fillStyle = colour;
+
+	var font_size = c.width * 0.13;
 	var font = font_size.toString() + "px basicWoodlands";
 
-	ctx.font = font; 
-	
+	ctx.font = font;
+
 	ctx.fillText(score, c.width * 0.95, c.width * 0.05);
-	ctx.fillText(highscore, c.width * 0.95, c.width * 0.16); 
+	ctx.fillText(highscore, c.width * 0.95, c.width * 0.16);
 }
 
 function startGame() {
 	var swayGame = new Sway('myCanvas');
-	
+
 	swayGame.background();
 	swayGame.initVariable();
-	
+
 	swayGame.getCnv().addEventListener('click', function(event) {handleClick(event.x * window.devicePixelRatio, event.y * window.devicePixelRatio, swayGame);});
-	
+
 	setInterval(function() {
 		changeColours(PRIMARY_COLOUR, DEST_COLOUR);
-		
+
 		swayGame.background();
-		
+
 		swayGame.getPen().setCol(PRIMARY_COLOUR);
-		
+
 		if (swayGame.getOldHeight() != swayGame.getCnv().height) {
 			swayGame.initVariable();
 			swayGame.setOldHeight(swayGame.getCnv().height);
-		}  
-		
-		drawPauseButton(swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.10, 
+		}
+
+		drawPauseButton(swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.10,
 						SECONDARY_COLOUR, swayGame.getCtx());
-		
-		updateScore(swayGame.getCnv(), swayGame.getCtx(), swayGame.getScore(), 0, SECONDARY_COLOUR);
-		
+
+		updateScore(swayGame.getCnv(), swayGame.getCtx(), swayGame.getScore(), window.localStorage.getItem('highscore'), SECONDARY_COLOUR);
+
 		swayGame.manageOrbs();
-		
+
 		if (swayGame.getStatus() == SLOW_DOWN) {
 			if (!swayGame.getPen().startShrink()) {
 				swayGame.setStatus(REGULAR);
@@ -377,7 +385,7 @@ function startGame() {
 			swayGame.setScore(0);
 			swayGame.setStatus(REGULAR);
 		}
-		
+
 		swayGame.move();
 	}, TIME_INTERVAL);
 }
