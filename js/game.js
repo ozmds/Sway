7f6892fce10973ed848335a5841ab64ee318e06c
Athavan Karunakaran <athavan.karunakaran@gmail.com>
2017-07-29 19:41:12 -0400

    Continued Re-Factoring

--- js/game.js
+++ js/game.js
@@ -12,6 +12,60 @@ class Game {
         this.cnv.style.left = (MARGIN).toString() + 'px';
     }
 
+    getStatus() {
+		/* Return Status */
+		return this.status;
+	}
+
+	setStatus(x) {
+		/* Set Status to x */
+		this.status = x;
+	}
+
+	getScore() {
+		/* Return Score */
+		return this.score;
+	}
+
+	setScore(x) {
+		/* Set Score to x */
+		this.score = x;
+	}
+
+	incrementScore() {
+		/* Increase Score By 1 */
+		this.score += 1;
+
+		if (this.score > window.localStorage.getItem('highscore')) {
+			window.localStorage.setItem('highscore', this.score);
+		}
+	}
+
+	getOldHeight() {
+		/* Return Old Height */
+		return this.old_height;
+	};
+
+	setOldHeight(x) {
+		/* Set Old Height to x */
+		this.old_height = x;
+	}
+
+	getCtx() {
+		/* Return Context */
+		return this.ctx;
+	}
+
+	getCnv() {
+		/* Return Canvas */
+		return this.cnv;
+	}
+
+	getPen() {
+		/* Return Pendulum */
+		return this.pen;
+	}
+
     background() {
         this.cnv.height = (window.innerHeight - 10) * window.devicePixelRatio;
     	this.cnv.width = (window.innerWidth - 10) * window.devicePixelRatio;
@@ -55,7 +109,97 @@ class Game {
 		if (window.localStorage.getItem('highscore') == null) {
 			window.localStorage.setItem('highscore', this.score);
 		}
-
-        this.pen.draw();
     }
+
+    move() {
+		/* Move the Pendulum as a Whole */
+		this.pen.move(this.status);
+		this.pen.draw();
+	}
+
+	incrementPenSpeed(score) {
+		/* Increase Speed of Pendulum */
+		if ((score % 10 == 0) && (score <= 100)) {
+			pen_time = PEN_START_TIME - (score / 10) * PEN_DECREMENT_TIME;
+			this.pen.calcPenSpeed(pen_time);
+		}
+	}
+}
+
+function changeColours(colour1, colour2) {
+	/* Slowly return fading colours from colour1 to colour2 */
+	var i;
+
+	var c1 = [];
+	var c2 = [];
+
+	var newcol = []
+
+	for (i = 0; i < 3; i++) {
+		c1.push(parseInt(colour1.slice(2 * i + 1, 2 * i + 3), 16));
+		c2.push(parseInt(colour2.slice(2 * i + 1, 2 * i + 3), 16));
+
+		if (c1[i] > c2[i]) {
+			newcol.push(c1[i] - 1);
+		} else if (c1[i] == c2[i]) {
+			newcol.push(c1[i]);
+		} else {
+			newcol.push(c1[i] + 1);
+		}
+
+		if (newcol[i] == 0) {
+			newcol[i] = '00';
+		} else if (newcol[i] < 16) {
+			newcol[i] = '0' + newcol[i].toString(16);
+		}
+	}
+
+	PRIMARY_COLOUR = '#' + newcol[0].toString(16) + newcol[1].toString(16) + newcol[2].toString(16);
+
+	return true;
+}
+
+function handleClick(event_x, event_y, sway) {
+	/* React to a click */
+    if (STATE == PAUSE) {
+        STATE = GAME;
+    } else if (event_x > pausex && pausex + pauseside > event_x) {
+        if (event_y > pausey && pausey + pauseside > event_y) {
+            STATE = PAUSE;
+        }
+    } else {
+        sway.getPen().getPen().flip();
+
+    	if (sway.getStatus() != BALLOON) {
+    		sway.getPen().getSPen().flip();
+    	}
+    }
+}
+
+function drawPauseButton(x, y, side_len, colour, context) {
+	/* Draw a Pause Button */
+    pausex = x;
+    pausey = y;
+    pauseside = side_len;
+
+	var bar_width = side_len / 3;
+	context.fillStyle = colour;
+
+	context.fillRect(x, y, bar_width, side_len);
+	context.fillRect(x + 2 * bar_width, y, bar_width, side_len);
+}
+
+function updateScore(c, ctx, score, highscore, colour) {
+	/* Write the score on the canvas */
+	ctx.textBaseline = 'middle';
+	ctx.textAlign = 'end';
+	ctx.fillStyle = colour;
+
+	var font_size = c.width * 0.13;
+	var font = font_size.toString() + "px basicWoodlands";
+
+	ctx.font = font;
+
+	ctx.fillText(score, c.width * 0.95, c.width * 0.05);
+	ctx.fillText(highscore, c.width * 0.95, c.width * 0.16);
 }
--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ function startGame() {
 
     swayGame.background();
     swayGame.initVariable();
+
+    swayGame.getCnv().addEventListener('click', function(event) {handleClick(
+        event.x * window.devicePixelRatio, event.y * window.devicePixelRatio, swayGame);});
+
+        drawPauseButton(swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.10,
+                        SECONDARY_COLOUR, swayGame.getCtx());
+
+        updateScore(swayGame.getCnv(), swayGame.getCtx(), swayGame.getScore(), window.localStorage.getItem('highscore'), SECONDARY_COLOUR);
 }
