c7345d4d8450c8682ddca60bc295ae599d3b94c2
Athavan Karunakaran <athavan.karunakaran@gmail.com>
2017-07-30 11:28:50 -0400

    Started writing to orblist.js

--- js/main.js
+++ js/main.js
@@ -9,8 +9,14 @@ function startGame() {
     swayGame.getCnv().addEventListener('click', function(event) {handleClick(
         event.x * window.devicePixelRatio, event.y * window.devicePixelRatio, swayGame);});
 
+    setInterval(function() {
+        swayGame.background();
+        
         drawPauseButton(swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.04, swayGame.getCnv().width * 0.10,
-                        SECONDARY_COLOUR, swayGame.getCtx());
+                            SECONDARY_COLOUR, swayGame.getCtx());
 
         updateScore(swayGame.getCnv(), swayGame.getCtx(), swayGame.getScore(), window.localStorage.getItem('highscore'), SECONDARY_COLOUR);
+
+        swayGame.move();
+    }, TIME_INTERVAL);
 }
